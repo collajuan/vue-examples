083fb9d202efc859b708d96133d0b87536159ff5
collajuan <collajuan@gmail.com>
2024-10-19 10:53:47 +0200

    methods

--- src/examples/07-ExerciseModifyVariables.vue
+++ src/examples/07-ExerciseModifyVariables.vue
@@ -1,11 +1,32 @@
 <script>
 export default {
   data() {
-    return {}
-  }
+    return {
+      texto: 'holaa',
+      textos: ['uno','dos', 'tres'],
+      };
+    },
+      methods: {
+        handleClick() {
+          console.log(Math.floor(Math.random()*this.textos.length));          
+          this.texto = this.textos[Math.floor(Math.random()*this.textos.length )]
+        }
+      }
 }
 </script>
 
 <template>
-  <p>This is a sandbox, start creating your exercise!</p>
+  <button @click="handleClick">{{ texto }}</button>
+  <p @mouseover="handleClick">{{ texto }}</p>
 </template>
+
+
+<style scoped>
+  button {
+    background-color: aqua;
+  }
+  p {
+    background-color: bisque
+  }
+
+</style>  
